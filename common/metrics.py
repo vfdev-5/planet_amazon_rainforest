2e94bf50e0608364772abdb709ced8b54ca7ee10
vfdev <uids3502@contiwan.com>
2017-07-13 10:37:35 +0200

    * Update floydscript to predict on newly pretrained models
    * Rename old weights
    
    *[WIP] Update prediction blender

--- common/metrics.py
+++ common/metrics.py
@@ -4,4 +4,5 @@ from sklearn.metrics import fbeta_score
 
 
 def score(y_true, y_pred):
-    return fbeta_score(y_true, y_pred, beta=2, average='samples')
+    average = 'samples' if len(y_true.shape) == 2 else 'binary'
+    return fbeta_score(y_true, y_pred, beta=2, average=average)
